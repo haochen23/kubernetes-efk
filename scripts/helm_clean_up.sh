f93588e4a2dbb9321ef8c5b5fd9fd3c29c4da823
Hao Chen <hao.chen23@outlook.com>
2022-02-25 16:17:43 +1300

    Add scripts for helm efk installation

---
+++ scripts/helm_clean_up.sh
@@ -0,0 +1,4 @@
+helm uninstall efk
+sudo rm -rf /mnt/ataicmap02/CCTV_ProVision/efk/nodes/
+sudo rm -rf /mnt/ataicmap02/CCTV_ProVision/efk2/nodes/
+sudo rm -rf /mnt/ataicmap02/CCTV_ProVision/efk3/nodes/
---
+++ scripts/helm_install_efk.sh
@@ -0,0 +1,1 @@
+helm upgrade efk --install efk/
